0bdfa9637901cd94f3b9b61eac215a67bc7d25b5
우다원 <105121244+woodawon@users.noreply.github.com>
2023-01-16 22:53:20 +0900

    First commit to connect with Github.

--- app/src/main/java/com/example/kotproject/MainActivity.kt
+++ app/src/main/java/com/example/kotproject/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.kotproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        Toast("wow");
+    }
+
+    private fun Toast(s: String) {
+
     }
 }
