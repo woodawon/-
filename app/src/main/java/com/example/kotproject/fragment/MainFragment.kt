49ce4d50b8839af235039d0602becf98f4772681
우다원 <105121244+woodawon@users.noreply.github.com>
2023-01-18 23:37:26 +0900

    main, question xml 완성 및 fragment 코드 추가중..

--- app/src/main/java/com/example/kotproject/fragment/MainFragment.kt
+++ app/src/main/java/com/example/kotproject/fragment/MainFragment.kt
@@ -5,56 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.example.kotproject.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MainFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MainFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    lateinit var navController : NavController; // lateinit : 지연 초기화 예약어 -> 불필요한 지속적인 초기화 과정으로 인한 메모리&실행 시간 소비를 최대한 막기 위해서 사용.
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        savedInstanceState: Bundle? // Bundle : 클래스
+    ): View? { // View : 클래스
+        return inflater.inflate(R.layout.fragment_main, container, false) // inflate : layoutinflator클래스 -> public View inflate{}
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MainFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MainFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState) // super -> Fragment 클래스
+
+        navController = Navigation.findNavController(view); // Navigation : Object -> findNavController() 함수 사용.
+
+
     }
+
 }
